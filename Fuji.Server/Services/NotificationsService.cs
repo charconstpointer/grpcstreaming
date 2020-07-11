1428632e0516893f92d24d6544f3cf1ccc88b62c
charconstpointer <michalolszewskidev@gmail.com>
2020-07-11 02:45:28 +0200

    init

---
+++ Fuji.Server/Services/NotificationsService.cs
@@ -0,0 +1,35 @@
+﻿using System.Linq;
+using System.Threading.Tasks;
+using Grpc.Core;
+using Microsoft.Extensions.Logging;
+using Worker.Schedule;
+
+namespace Fuji.Server.Services
+{
+    public class NotificationsService : Notifications.NotificationsBase
+    {
+        private readonly ILogger<NotificationsService> _logger;
+
+        public NotificationsService(ILogger<NotificationsService> logger)
+        {
+            _logger = logger;
+        }
+
+        public override async Task Listen(IAsyncStreamReader<ListenRequest> requestStream,
+            IServerStreamWriter<ListenResponse> responseStream, ServerCallContext context)
+        {
+            while (await requestStream.MoveNext())
+            {
+                var request = requestStream.Current;
+                _logger.LogInformation($"Received request for {request.Count} messages");
+                for (var i = 0; i < request.Count; i++)
+                {
+                    await responseStream.WriteAsync(new ListenResponse {Payload = i});
+                    _logger.LogInformation($"Writing {i+1} of {request.Count} messages");
+                }
+
+                _logger.LogInformation("Listening...");
+            }
+        }
+    }
+}
---
+++ Fuji/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Threading.Tasks;
+using Grpc.Core;
+using Grpc.Net.Client;
+using Worker.Schedule;
+
+namespace Fuji
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            var channel = GrpcChannel.ForAddress("https://localhost:5001");
+            var client = new Notifications.NotificationsClient(channel);
+            using var call = client.Listen();
+            var _ = Task.Run(async () =>
+            {
+                while (await call.ResponseStream.MoveNext())
+                {
+                    var note = call.ResponseStream.Current;
+                    Console.WriteLine("Received " + note);
+                }
+            });
+            while (true)
+            {
+                if (int.TryParse(Console.ReadLine(), out var buffer))
+                {
+                   await call.RequestStream.WriteAsync(new ListenRequest {Count = buffer}); 
+                }
+                else
+                {
+                    await call.RequestStream.WriteAsync(new ListenRequest {Count = 10}); 
+                }
+            }
+        }
+    }
+}
